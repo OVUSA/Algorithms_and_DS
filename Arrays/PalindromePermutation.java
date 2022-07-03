356287f8677cf2dea4c8aed6725896ae6e26289b
OVUSA <ovelichko@luc.edu>
2022-07-02 22:52:47 -0500

    added second array challenge

--- Arrays/PalindromePermutation.java
+++ Arrays/PalindromePermutation.java
@@ -1,4 +1,19 @@
 package CrackingCodingInterview.Arrays;
 
+import java.util.Scanner;
+
+//Tack Coa
 public class PalindromePermutation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the phrase");
+        String st = sc.next();
+
+    }
+
+    public static boolean checkPalindomePermut(String st ){
+
+        return false;
+
+    }
 }

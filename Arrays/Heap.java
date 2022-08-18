ec61c6f78a2489a17873340575add5a12aeede08
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2022-08-17 22:12:52 -0500

    Create Heap.java

---
+++ Arrays/Heap.java
@@ -0,0 +1,33 @@
+package CrackingCodingInterview.Arrays;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Heap {
+    public static void main(String[] args) {
+        int [] arr = new int [] {2,7,4,1,8,1};
+        System.out.println(lastStoneWeight(arr));
+
+    }
+
+    public static int lastStoneWeight(int[] stones) {
+
+        Arrays.sort(stones);
+
+        PriorityQueue<Integer> qu = new PriorityQueue<>();
+        for(int i : stones){qu.add(i);}
+
+        while(qu.size()>1){
+            int x = qu.poll();
+            int y = qu.poll();
+
+            if(x!=y){
+                y=y-x;
+                qu.add(y);
+            }
+        }
+
+        return qu.peek();
+
+    }
+}

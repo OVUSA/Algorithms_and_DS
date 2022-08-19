f6dfb37f38527cb2b3f3ee49857a75f9d12b0f29
OVUSA <ovelichko@luc.edu>
2022-08-18 20:00:09 -0500

    somewhat resolve fibonacci

--- FibonacciVar/minCostClimbingStairs.java
+++ FibonacciVar/minCostClimbingStairs.java
@@ -3,25 +3,24 @@ package CrackingCodingInterview.FibonacciVar;
 public class minCostClimbingStairs {
     public static void main(String[] args) {
         int [] cost = new int [] {10,15,20};
-        minCostClimbingStairs(cost);
-
+        System.out.println(minCostClimbingStairs(cost));
 
     }
     public static int minCostClimbingStairs(int[] cost) {
-        int pay = 0;
-        if(cost.length==2){pay = Math.min(cost[0],cost[1]);
+        int first = cost[0];
+        int second = cost[1];
+        int cur = 0;
+        if(cost.length==2){cur = Math.min(cost[0],cost[1]);}
 
+        for(int i = 2;i<cost.length;i++){
+            cur = cost[i]+Math.min(first,second);
+            first = second;
+            second=cur;
 
-
-        for(int i = 0;i<=cost.length;i++){
-            pay = Math.min()
         }
 
 
-
-
-
-        return pay;
+        return Math.min(first,second);
 
     }
 }

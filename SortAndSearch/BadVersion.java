af1bf5bf087bf5481693a2b68feed9f95bb01963
OVUSA <ovelichko@luc.edu>
2022-08-22 15:34:55 -0500

    20>

--- SortAndSearch/BadVersion.java
+++ SortAndSearch/BadVersion.java
@@ -1,31 +1,32 @@
 package Algorithms_and_DS.SortAndSearch;
 
 public class BadVersion {
-
+// every version after bad one is bad , find the very first one
     public static void main(String[] args) {
+        System.out.println(firstBadVersion(20));
 
     }
     public static int firstBadVersion(int n) {
-        int start = 0;int end = n;
-        if(n ==1){
-            return 1;
-        }else {
-            while(start<=end){
-                int mid = start+(end-start)/2;
-                if(isBadVersion(mid)){helper(n,);}//right side
-                if()
+
+            int start = 0; int end =n;
+            int res = n;
+
+            if(n==1){return 1;}
+            while(start<=end) {
+                int mid = start + (end - start) / 2;
+                if (!isBadVersion(mid)) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                    res = Math.min(res, mid);
+                }
             }
+            return res;
         }
 
-    }
-
-    public int helper ( int n, int start, int end){
-
-    }
-
-    public boolean isBadVersion( int i){
-        int badVersion = 5;
-        return i==badVersion;
+    public static boolean isBadVersion(int i){
+        int badVersion = 18;
+        return i>=badVersion;
 }
 
 }

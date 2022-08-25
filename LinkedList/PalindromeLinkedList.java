165004392c909c38aeba95f629cd00c08413296f
OVUSA <ovelichko@luc.edu>
2022-08-24 19:45:59 -0500

    comparing beg and the end of the list allows to determine the palindrome

--- LinkedList/PalindromeLinkedList.java
+++ LinkedList/PalindromeLinkedList.java
@@ -4,46 +4,30 @@ import java.util.Stack;
 
 public class PalindromeLinkedList {
     public static void main(String[] args) {
-        // find if linked list is Palindrome
-        //[1,2,2,1] or [1,2]
+// the palindrome is something START and ENDS the same way ,
 
 
     }
         public boolean isPalindrome(ListNode head) {
             Stack<Integer> nums = new Stack<>();
 
-            while(head!=null){
-                if(nums.isEmpty()){
-                    nums.add(head.val);
-                    System.out.println("Initiate stack");
-                    print(nums);
-                }else if(nums.peek()!=head.val){
-                    nums.add(head.val);
-                    System.out.println("Add into the stack");
-                    print(nums);
-                }else if(nums.peek()==head.val){
+            ListNode temp = head;
+            while(temp!=null){
+                nums.add(temp.val);
+                temp = temp.next;
+            }
+
+            temp = head;
+            while(temp!=null){
+                if(temp.val != nums.peek()){
+                    return false;
+                }else{
                     nums.pop();
-                    System.out.println("Delete from the stack");
-                    print(nums);
-
                 }
-                head = head.next;
+                temp= temp.next;
             }
 
-            return nums.isEmpty();
-
+            return true;
         }
 
-    public void print(Stack<Integer> st) {
-        if (st.isEmpty()) {
-            System.out.println("Is EMPTY!");
-
-        } else {
-
-            for (Integer i : st) {
-                System.out.println(i);
-            }
-
-        }
-    }
 }

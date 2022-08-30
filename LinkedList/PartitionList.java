8593182530600f82191e61628cb5b270d0e97277
OVUSA <ovelichko@luc.edu>
2022-08-29 21:31:30 -0500

    version 1

--- LinkedList/PartitionList.java
+++ LinkedList/PartitionList.java
@@ -6,18 +6,25 @@ public class PartitionList {
         ListNode second = head;
         ListNode temp = first;
 
+        //find a node with x value
+        while (first.val != x) {
+            first = first.next;
 
-        while(second.next!=null|| first!=null){
-            if(first.val == x){
-                if(first.next.val<= x){
-                    temp = first.next;
-                    first.next = first.next.next;
+        }
+        //iterate until the end of the list
+        while (first != null) {
+            if (first.next.val <= x) {
+                temp = first.next;
+                first.next = first.next.next;
 
-                    temp.next = second.next;
-                    second.next = temp;
-                    temp= temp.next;
+                temp.next = second.next;
+                second.next = temp;
+                temp = temp.next;
 
-                }
             }
+            first = first.next;
+
+        }
+        return head;
     }
 }
